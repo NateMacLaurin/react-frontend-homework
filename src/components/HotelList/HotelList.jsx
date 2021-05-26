0991ac4994992f7efb19be5a81de320955ce3404
NateMacLaurin <blackmagenate@gmail.com>
2021-05-26 00:38:20 -0500

    added debug and testing dev console log info on App component

--- src/components/HotelList/HotelList.jsx
+++ src/components/HotelList/HotelList.jsx
@@ -1,34 +1,12 @@
 import React from 'react';
+import HotelCard from '../HotelCard';
 
 const HotelList = ( {hotels} ) => {
     return (
         <div className="hotel-list">
             {hotels.map(hotel => (
                 <div className="hotel-card" key={hotel.id}>
-                    <div
-                        className="image"
-                        style={{ backgroundImage: `url(${hotel.hotelStaticContent.mainImage.url})`}}
-                    >
-                    </div>
-                    <div className="hotel-details">
-                        <div className="hotel-name">
-                            {hotel.hotelStaticContent.name}
-                        </div>
-                        <div className="location">
-                            {hotel.hotelStaticContent.neighborhoodName}
-                        </div>
-                    </div>
-                    <div className="price-details">
-                        <span className="price">
-                            <span dangerouslySetInnerHTML={{ __html: hotel.lowestAveragePrice.symbol }}>
-                            </span>
-                            {hotel.lowestAveragePrice.amount}
-                        </span>
-                        <span className="rewards">
-                            {hotel.rewards.miles} miles
-                        </span>
-                        <button className="button">Select</button>
-                    </div>
+                    <HotelCard hotel={hotel} />
                 </div>
                 )
             )}
