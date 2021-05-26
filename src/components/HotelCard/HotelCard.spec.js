d7e76b0ca65f3f13cc4629aa2ca942a2c3181a61
NateMacLaurin <blackmagenate@gmail.com>
2021-05-26 01:18:40 -0500

    added test API data on props to HotelList and HotelCard jest render tests

--- src/components/HotelCard/HotelCard.spec.js
+++ src/components/HotelCard/HotelCard.spec.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import HotelCard from './HotelCard';
+import MockData from '../../services/hotel-mock-api-data';
 
 describe('HotelCard', () => {
-    const wrapper = shallow(<HotelCard />);
+    const wrapper = shallow(<HotelCard hotel={MockData.results.hotels[0]}/>);
 
-    it('renders the HotelCard component', () => {
-        expect(wrapper.find('.hotel-card').exists()).toBe(true);
+    it('renders the HotelCard component with test API data on props', () => {
+        expect(wrapper.find('.hotel-details').exists()).toBe(true);
     });
 });
